101ccc5f713814f1afc3707b6dde1fb049e16cfb
MuhammadSodiq571 <musozonovmuhammadsodik@gmail.com>
2024-11-20 20:25:00 +0500

    toggle-polybar.sh

---
+++ .config/bspwm/scripts/toggle-polybar.sh
@@ -0,0 +1,10 @@
+pgrep -x polybar
+STATUS=$?
+
+if test $STATUS -eq 0 
+then
+  kill $(pgrep polybar) && bspc config -m focused top_padding 0
+else 
+  $HOME/.config/polybar/launch.sh && bspc config -m focused top_padding 41
+fi
+
