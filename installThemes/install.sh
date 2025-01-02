ef915e44b1ea08ac2b56bd4daeb5c3c8abe8f02b
MuhammadSodiq071 <musozonovmuhammadsodik@gmail.com>
2025-01-02 18:34:37 +0500

    update

--- installThemes/install.sh
+++ installThemes/install.sh
@@ -1,3 +1,3 @@
-tar -xvf ArchCursorTheme.tar.tar
-sudo mv ArchCursorTheme /usr/share/icons/
+tar -xvf ~/Linux-config/installThemes/ArchCursorTheme.tar.tar
+sudo mv ~/Linux-config/installThemes/ArchCursorTheme /usr/share/icons/
 sudo pacman -S lxappearance qt6ct arc-gtk-theme papirus-icon-theme
